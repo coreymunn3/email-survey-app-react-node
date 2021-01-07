e00a5794d014cd3745ca5eee23ac444f1b4f3e29
Michael Corey Munn <coreymunn3@gmail.com>
2021-01-07 10:48:19 -0500

    added google Oauth beginning setup

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const app = express();
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello, This is the default message.' });
-});
+
+// OAuth Setup
+const GOOGLE_OAUTH_CLIENT_ID = process.env.GOOGLE_OAUTH_CLIENT_ID;
+const GOOGLE_OAUTH_CLIENT_SECRET = process.env.GOOGLE_OAUTH_CLIENT_SECRET;
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_OAUTH_CLIENT_ID,
+      clientSecret: GOOGLE_OAUTH_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      //console.log(accessToken);
+      console.log(profile);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 
