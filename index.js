afb653c2394068a40c11d06335e0db48a370d725
Michael Corey Munn <coreymunn3@gmail.com>
2021-01-09 13:28:17 -0500

    added logic to strategy callback, implemented serializing and deserializing. Added cookie session library and some middleware for passport in index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
@@ -12,8 +14,19 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 const app = express();
-app.get('/', (req, res) => res.send('Welcome to the App'));
+app.use(
+  cookieSession({
+    // cookie age in MS
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    // key to encrypt cookie
+    keys: [process.env.COOKIE_KEY],
+    name: 'session',
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/auth/google', require('./routes/authRoutes'));
+app.get('/', (req, res) => res.send('Welcome to the App'));
 
 const PORT = process.env.PORT || 5000;
 
--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,18 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/Users');
 
+// turn mongoose model instance into a cookie
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// turn cookie back into model instance
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
 // OAuth Setup
 const GOOGLE_OAUTH_CLIENT_ID = process.env.GOOGLE_OAUTH_CLIENT_ID;
 const GOOGLE_OAUTH_CLIENT_SECRET = process.env.GOOGLE_OAUTH_CLIENT_SECRET;
@@ -13,9 +25,18 @@ passport.use(
       clientSecret: GOOGLE_OAUTH_CLIENT_SECRET,
       callbackURL: '/auth/google/callback',
     },
-    async (accessToken, refreshToken, profile, cb) => {
+    async (accessToken, refreshToken, profile, done) => {
       // create a new user in db
-      await new User({ googleId: profile.id }).save();
+      const user = await User.findOne({ googleId: profile.id });
+      if (!user) {
+        const newUser = new User({ googleId: profile.id });
+        await newUser.save();
+        console.log('New User Created');
+        done(null, user);
+      } else {
+        console.log('User already exists');
+        done(null, user);
+      }
     }
   )
 );
