fb4149b26819378aef3340dbe193d57547b2276d
Michael Corey Munn <coreymunn3@gmail.com>
2021-01-12 11:37:46 -0500

    client side setup with redux and thunk. Changed Favicon

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='/auth/google'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Sign in With Google
-        </a>
-        <a href='/api/currentUser'>Current User</a>
-        <a href='/api/logout'>Log Out</a>
-      </header>
+    <div>
+      <h1>This is the Application!</h1>
     </div>
   );
-}
+};
 
 export default App;
