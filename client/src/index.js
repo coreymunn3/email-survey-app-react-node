05251eb608218901eb43d06d6237cc9687111481
Michael Corey Munn <coreymunn3@gmail.com>
2021-01-26 11:44:05 -0500

    added /logonlyinproduction to compose with devtools import for redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 import thunkMiddleware from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 import App from './App';
 import './index.css';
 // stripe
