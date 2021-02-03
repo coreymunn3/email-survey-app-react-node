6ab466bbeeb9926cfb5ed99037bd1c17c1cff50d
Michael Corey Munn <coreymunn3@gmail.com>
2021-02-03 16:26:22 -0500

    added form state and error validation to form moving from first to second step. Recipients validation not complete.

--- client/src/components/pages/surveyForm/SurveyForm.js
+++ client/src/components/pages/surveyForm/SurveyForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import {
   Container,
@@ -8,9 +9,10 @@ import {
   Step,
   StepLabel,
   Button,
+  Tooltip,
 } from '@material-ui/core';
 import Confirmation from './Confirmation';
-import SurveyFormStep from './SurveyFormStep';
+import SurveyFormSteps from './SurveyFormSteps';
 
 const useStyles = makeStyles({
   container: {
@@ -32,12 +34,33 @@ const useStyles = makeStyles({
 
 const SurveyForm = () => {
   const classes = useStyles();
-  const [activeStep, setActiveStep] = useState(0);
+  // stepper
   const steps = ['Create Survey', 'Review & Submit'];
-
+  const [activeStep, setActiveStep] = useState(0);
+  const isFirstStep = activeStep === 0;
   const isEndState = activeStep === steps.length;
   const nextStep = () => setActiveStep((prevStep) => prevStep + 1);
   const backStep = () => setActiveStep((prevStep) => prevStep - 1);
+  // form state
+  const [formData, setFormData] = useState({});
+  // form validation
+  const validate = (values) => {
+    const errors = {};
+    if (!values.title) {
+      errors.title = 'You must provide an email title';
+    }
+    if (!values.subject) {
+      errors.subject = 'You must provide an email subject';
+    }
+    if (!values.body) {
+      errors.body = 'You must provide an email body';
+    }
+    if (!values.recipients) {
+      errors.recipients = 'You must provide at least one email recipients';
+    }
+    return errors;
+  };
+  const errorsExist = Object.keys(validate(formData)).length > 0;
 
   return (
     <Container className={classes.container}>
@@ -62,7 +85,12 @@ const SurveyForm = () => {
           isEndState ? (
             <Confirmation />
           ) : (
-            <SurveyFormStep activeStep={activeStep} />
+            <SurveyFormSteps
+              activeStep={activeStep}
+              formData={formData}
+              setFormData={setFormData}
+              errors={validate(formData)}
+            />
           )
         }
         {
@@ -76,28 +104,44 @@ const SurveyForm = () => {
                 color='primary'
                 onClick={() => setActiveStep(0)}
               >
-                Return
+                Back To My Surveys
               </Button>
             </Container>
           ) : (
             <Container className={classes.buttonGroup}>
               <Button
+                component={Link}
+                to='/surveys'
+                className={classes.button}
+                variant='outlined'
+              >
+                Cancel
+              </Button>
+
+              <Button
                 className={classes.button}
                 variant='contained'
                 color='primary'
                 onClick={backStep}
-                disabled={activeStep === 0}
+                disabled={isFirstStep}
               >
                 Back
               </Button>
-              <Button
-                variant='contained'
-                color='primary'
-                onClick={nextStep}
-                className={classes.button}
+              <Tooltip
+                title={errorsExist ? 'You Must Complete All Fields' : ''}
               >
-                {activeStep === steps.length - 1 ? 'Submit' : 'Next'}
-              </Button>
+                <span>
+                  <Button
+                    variant='contained'
+                    color='primary'
+                    disabled={errorsExist}
+                    onClick={nextStep}
+                    className={classes.button}
+                  >
+                    {activeStep === steps.length - 1 ? 'Submit' : 'Next'}
+                  </Button>
+                </span>
+              </Tooltip>
             </Container>
           )
         }
--- client/src/components/pages/surveyForm/SurveyFormSteps.js
+++ client/src/components/pages/surveyForm/SurveyFormSteps.js
@@ -1,17 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Container, Grid, TextField } from '@material-ui/core';
 
-const SurveyFormStep = ({ activeStep }) => {
+const SurveyFormSteps = ({ activeStep, formData, setFormData, errors }) => {
   const isReview = activeStep === 1;
-  // for testing...
-  const [field, setField] = useState('');
 
   const FIELDS = [
     { id: 'title', label: 'Survey Title', multiline: false, rows: 0 },
     { id: 'subject', label: 'Subject Line', multiline: false, rows: 0 },
-    { id: 'body ', label: 'Email Body', multiline: true, rows: 5 },
+    { id: 'body', label: 'Email Body', multiline: true, rows: 5 },
     { id: 'recipients', label: 'Recipient List', multiline: true, rows: 1 },
   ];
+
+  const handleChange = (e) =>
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
   return (
     <Container>
       <Grid container spacing={2}>
@@ -24,9 +28,12 @@ const SurveyFormStep = ({ activeStep }) => {
               multiline={multiline}
               rows={rows}
               fullWidth
+              required
               variant={isReview ? 'filled' : 'outlined'}
               InputLabelProps={{ shrink: true }}
               disabled={isReview}
+              value={formData[id] || ''}
+              onChange={handleChange}
             />
           </Grid>
         ))}
@@ -35,4 +42,4 @@ const SurveyFormStep = ({ activeStep }) => {
   );
 };
 
-export default SurveyFormStep;
+export default SurveyFormSteps;
